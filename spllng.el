41e7b569c1d69d631fe47c2a7a7b7f0f16d3b087
Lars Ingebrigtsen <larsi@gnus.org>
2025-09-30 00:10:35 +0200

    Scaffolding

---
+++ spllng.el
@@ -0,0 +1,28 @@
+;;; spllng.el --- Check spelling -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Free Software Foundation, Inc.
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: wordpress, blogs
+
+;; spllng is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; spllng is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+
+
+
+(provide 'spllng)
+
+;;; spllng.el ends here
